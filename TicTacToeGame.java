d41b643e3eb47df1b66af8ba58ceab5cf4a3a63e
Ahsan <ahsan.moh426@gmail.com>
2025-03-31 20:48:44 -0400

    Tic tac toe

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -47,7 +47,7 @@ public class TicTacToeGame{
         
         return z;
 	}
-	public static void main(String[] args){
+	public static void main(String[]args){
 // This in array is drawing up the board lines. In (xstart, ystart, xend, yend) format
 // Top-Left corner of app window is (0,0) / Bottom-Right corner of app window is (600, 600)
 		int[][]boardLine = {{0,200,600,200},
@@ -59,118 +59,147 @@ public class TicTacToeGame{
 		for (int i=0;i<arr.length;i++)
 			for (int j=0;j<arr[i].length;j++)
 				arr[i][j]='-';
-String[][] spots = {{"top left", "top middle","top right"},
+        String[][] spots = {{"top left", "top middle","top right"},
 					{"middle left","the middle","middle right"},
 					{"bottom left","bottom middle","bottom right"}};
-Scanner read = new Scanner(System.in);
+//intializations
+        String kitaPlayers;
+        Scanner read = new Scanner(System.in);
 //menu 
-System.out.println("Welcome to Tic-Tac-Toe?");
-System.out.println("Would you like to play 1-player mode (1) or 2-player mode? (2)");
-String kitaPlayers = read.nextLine();
-int playrNum = 0;
-switch (kitaPlayers){
-	case "1":
-		playrNum = 1;
-	break;
+        System.out.println("Welcome to Tic-Tac-Toe?");
+        System.out.println("Would you like to play 1-player mode (1) or 2-player mode? (2)");
+        kitaPlayers = read.nextLine();
+        int playrNum = 0;
+        switch (kitaPlayers){
+	        case "1":
+		        playrNum = 1;
+	        break;
 
-	case "2":
-		playrNum = 2;
-	break;
+	        case "2":
+		        playrNum = 2;
+	        break;
 
-	default: 
-		System.out.println("There's been an error, please restart the code and be careful to write either 1 or 2");
-	break;
+	        default: 
+		        System.out.println("There's been an error, please restart the code and be careful to write either 1 or 2");
+	        break;
 }
 //THIS PART IS WHERE THE POPUP BEGINS
-TicTacToeBoard board = new TicTacToeBoard(620,720);
+        TicTacToeBoard board = new TicTacToeBoard(620,720);
 // This sets up the grid given the lines you provided above in the boardLine array
 		board.defineBoard(boardLine);
 // This will set your pictures to display for the x and o values. Make sure your names match exactly what you type in the parameters.
 		board.setFiles("x.png","o.jpg");
 // This sets up the spacing and place values for x and o
 		board.setBoard(arr);
-int game_continue = 1;
-int turn_counter = 0;
+//another initialization- NOTE: THE 2 MIGHT BE COMBINABLE BUT CHECK FIRST
+        int game_continue = 1;
+        int turn_counter = 0;
 int whose_turn = 0;
-int mess_up = 0;
-String choice1 = "dug";
-while (game_continue == 1){
-	if (playrNum == 1){
+        int mess_up = 0;
+        int mess_upRewrite = 0;
+        String choice1 = "dug";
+        while (game_continue == 1){
+        mess_up=0;
+        mess_upRewrite=0;
+	        if (playrNum == 1){
 
-	}
-	if (playrNum == 2){
-        if (mess_up != 0){
-            mess_up=0;
-        }
-		turn_counter++;
-		if(turn_counter%2 == 1){
-			whose_turn = 1;
-		}
-		else{
-			whose_turn = 2;
-		}
-	}
-	if(whose_turn == 1){
-		System.out.println("Where would Player 1 like to place an X?");
-	}
-	if(whose_turn == 2){
-		System.out.println("Where would Player 2 like to place an O?");
-	}
-		
-		for(int i=0;i<spots.length;i++){
-			for(int z=0;z<spots.length;z++){
-			System.out.print(spots[i][z]+"\t\t");
-			}
-			System.out.println("");
-		}
-		
-        while(mess_up==0){
-            choice1 = read.nextLine();
-            for(int i=0;i<spots.length;i++){
-                for(int z=0;z<spots.length;z++){
-                    if(choice1.equalsIgnoreCase(spots[i][z])){
-                        mess_up++;
+	        }
+	        if (playrNum == 2){
+		        turn_counter++;
+            }
+		    if(turn_counter%2 == 1){
+			    whose_turn = 1;
+		    }
+		    else{
+			    whose_turn = 2;
+		    }
+	        if(playrNum==2){
+	            if(whose_turn == 1){
+		            System.out.println("Where would Player 1 like to place an X?");
+	            }
+	            if(whose_turn == 2){
+		            System.out.println("Where would Player 2 like to place an O?");
+	            }
+            }
+            //prints out options
+		    for(int i=0;i<spots.length;i++){
+			    for(int z=0;z<spots.length;z++){
+			        System.out.print(spots[i][z]+"\t\t");
+			    }
+			    System.out.println("");
+		    }
+		//input loop
+        //
+        //ImportantImportantImportantImportantImportantImportantImportantImportantImportantImportant
+            while(mess_up==0 || mess_upRewrite!=0){
+                choice1 = read.nextLine();
+                mess_upRewrite=0;
+                for(int i=0;i<spots.length;i++){
+                    for(int z=0;z<spots.length;z++){
+                        if(choice1.equalsIgnoreCase(spots[i][z])){
+                            mess_up=1;
+                        }
                     }
                 }
-                
-            }
-            if(mess_up==0){
-                System.out.print("Sorry, you've probably made a typo, where would you  like to place your ");
-                if(whose_turn == 1){
-                    System.out.println("X?");
+                if(mess_up!=0){
+                    for(int i=0;i<spots.length;i++){
+                        for(int z=0;z<spots.length;z++){
+                            if((choice1.equalsIgnoreCase(spots[i][z]))&&(arr[i][z]=='x'||arr[i][z]=='o')){
+                                mess_upRewrite=1;
+                            }
+                        }
+                    }
                 }
-                if(whose_turn == 2){
-                    System.out.println("O?");
+                if(mess_up==0){
+                    System.out.print("Sorry, you've probably made a typo, where would you like to place your ");
+                    if(playrNum==2){
+                        if(whose_turn == 1){
+                            System.out.println("X?");
+                        }
+                        if(whose_turn == 2){
+                            System.out.println("O?");
+                        }
+                    }
+                    if(playrNum==1){
+                        System.out.println("X?");
+                    }
                 }
-            }
-            
-		
-       
+                if(mess_upRewrite==1){
+                    System.out.print("That spot has already been filled. Where would you like to place your ");
+                    if(playrNum==2){
+                        if(whose_turn == 1){
+                            System.out.println("X?");
+                        }
+                        if(whose_turn == 2){
+                            System.out.println("O?");
+                        }
+                    }
+                //
+                }
         }
+
 		for(int i=0;i<spots.length;i++){
 			for(int z=0;z<spots.length;z++){
 				if(choice1.equalsIgnoreCase(spots[i][z])){
 					if(whose_turn == 1){
 					    arr[i][z]='x';
 				    }
-				if(whose_turn == 2){
-					    arr[i][z] = 'o';
+				    if(whose_turn == 2){
+					    arr[i][z]='o';
 				    }
-			}
-		}
+			    }
+		    }
 		
-	}
+	    }
 	// This will refresh the board so that new elements will display.
-	board.repaint();
-	if (wincheck(arr)!=0){
-		System.out.println("Someone won!");
-		game_continue--;
-	}
-}
+        board.setBoard(arr);
+	    board.repaint();
+	    if (wincheck(arr)!=0){
+		    System.out.println("Someone won!");
+		    game_continue--;
+        }
+	    }
+        
+    }
 
-
-// This will refresh the board so that new elements will display.
-		board.repaint();
-
-	}
 }
